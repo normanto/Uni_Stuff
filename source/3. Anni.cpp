a0622d56bd34f238f4d275cc6d9e0bee8e2f7281
normanto <75088005+normanto@users.noreply.github.com>
2020-11-26 17:39:28 +0100

    complimento bello all'utente
    Dopo aver fatto tutto, dico pure che si porta bene l'età.

--- source/3. Anni.cpp
+++ source/3. Anni.cpp
@@ -6,4 +6,5 @@ int main()
 	cout << "Inserisci la tua eta' fratmo: ";
 	cin >> numero;
 	cout << "Impressione hai " << numero << " anni." << endl;
+	cout << "Fratello però te li porti bene." << endl;
 }
