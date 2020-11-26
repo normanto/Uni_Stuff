d78cc440000aae3ec7b92a9d89924f4ee2cc2222
normanto <75088005+normanto@users.noreply.github.com>
2020-11-26 18:35:51 +0100

    increible

--- source/3. Anni.cpp
+++ source/3. Anni.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main()	
+
 {	
 	int numero;
 	cout << "Inserisci la tua eta' fratmo: ";
-	cin >> numero;
+		cin >> numero;
+
 	cout << "Impressione hai " << numero << " anni." << endl;
-	cout << "Fratello però te li porti bene." << endl;
+		cout << "Fratello però te li porti bene." << endl;
 }
---
+++ source/4. Data di nascita.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ 
+    int giorno, mese, anno 
+    ;cout << "Inserisci il tuo giorno di nascita: ";
+        cin >> giorno; 
+
+    cout << "Inserisci il tuo mese di nascita: ";
+        cin >> mese; 
+
+    cout <<"Inserisci il tuo anno di nascita: ";
+        cin >> anno; 
+
+    cout << "Ua impressione, sei nato il " <<giorno <<"/" <<mese <<"/" <<anno; 
+}
+ 
+ 
