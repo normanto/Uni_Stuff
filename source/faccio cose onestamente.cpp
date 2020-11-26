4f0722ef8152ed3ab5830cbed1050201c1fc25aa
normanto <75088005+normanto@users.noreply.github.com>
2020-11-26 17:52:37 +0100

    questo è quello buono
    questo è quello buono

---
+++ source/faccio cose onestamente.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()	
+{	
+	int numero;
+	cout << "Inserisci la tua eta' fratmo: ";
+	cin >> numero;
+	cout << "Impressione hai " << numero << " anni." << endl;
+	cout << "Fratello però te li porti bene." << endl;
+}
