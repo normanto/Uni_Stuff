a1c1a4780a31980e70f3aa40d0486ff1d7085b89
normanto <75088005+normanto@users.noreply.github.com>
2020-11-26 17:31:13 +0100

    set-up

---
+++ source/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std
+;int main()
+{	
+	char tasto;
+	cout << "Inserisci un carattere e premi Invio" << endl;
+	cin >> tasto;
+	cout << "Grazie!" << endl << tasto << endl;
+}
---
+++ source/3. Anni.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()	
+{	
+	int numero;
+	cout << "Inserisci la tua eta' fratmo: ";
+	cin >> numero;
+	cout << "Impressione hai " << numero << " anni." << endl;
+}
