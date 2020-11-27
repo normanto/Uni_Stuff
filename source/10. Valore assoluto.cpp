e97848d381ebae0900f3880b8c567cbfec7df034
normanto <75088005+normanto@users.noreply.github.com>
2020-11-27 17:05:15 +0100

    Valore Assoluto

---
+++ source/10. Valore assoluto.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int num;
+        cout << "Inserire il numero: ";
+            cin >> num;
+    
+    if (num >= 0)
+        cout << "Il suo valore assoluto e': " << num;
+
+    else if (num < 0) 
+        cout << "Il suo valore assoluto e': " << -num;
+
+    cout << "\n\n";
+         
+    
+}
+        
