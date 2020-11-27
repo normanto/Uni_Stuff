b5beb9b4d135cbd38b78b72119f40e73eda2860a
normanto <75088005+normanto@users.noreply.github.com>
2020-11-27 14:40:01 +0100

    Somma e prodtto
    Con cifre date, mi mostra somma e prodotto delle suddette

---
+++ source/6. Somma e prodotto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a, b; 
+    cout << "Inserisci la prima cifra: ";
+        cin >> a;
+    
+    cout << "Inserisci la seconda cifra: ";
+        cin >> b;
+
+    cout << "La somma e' " << a+b << endl;
+    cout << "Il prodotto e' " << a*b << endl;
+    cout <<"\n\n";
+
+}
