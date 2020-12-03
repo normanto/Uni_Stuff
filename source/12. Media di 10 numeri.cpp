6e0535eea5029dcebb729ec503e062347430191b
normanto <75088005+normanto@users.noreply.github.com>
2020-12-03 16:16:35 +0100

    minor updates

---
+++ source/12. Media di 10 numeri.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int in;
+    float out, s = 0;
+
+        cout << "Calcola la media di 10 numeri."; 
+            cout << "\n\n";
+            
+        for (int i = 0; i < 10; i++) {
+            
+            cout << "Inserisci il numero: ";
+                cin >> in;
+        
+        s = s + in;
+    }
+        out = s / 10;
+
+        cout << "\nLa media e': " << out;
+            cout << "\n\n";
+}
