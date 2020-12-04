f73f01fc804d4b9a89ca5a9d4d6c713135889aed
normanto <75088005+normanto@users.noreply.github.com>
2020-12-04 15:31:23 +0100

    Somma, max e media

---
+++ source/13. Somma, max e media.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int num, max = 0, n = 0;
+    bool continua = true; 
+    float sum = 0, media;
+    
+    cout << "Inserisci una sequenza di numeri positivi. ";
+        cout << "\n";
+
+    do { 
+        cout << "Numero: ";
+            cin >> num;
+
+        if (num < 0)
+            continua = false;
+
+        else{
+                sum = sum + num;
+                if (num > max);
+                    max = num;
+            }
+                n = n + 1;
+                
+        }  
+            while (continua);
+
+            media = sum / n;
+        
+            cout << "Inseriti " << n << " valori: somma = " << sum << ", max = " << max << ", media = " << media;
+            cout << "\n\n";
+}
